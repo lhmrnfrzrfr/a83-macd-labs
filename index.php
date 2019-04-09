053e851d6bd48f331a5f3f37b00034c0dc918367
unknown <ilhamriana3@gmail.com>
2019-04-10 03:05:28 +0700

    fix css

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
  	h2 { font-size: 1.75em; }
  	h3 { font-size: 1.2em; }
  	table { margin-top: 0.75em; }
- 	th { font-size: 1.2em; text-align: left; border: none; padding-left: 20; }
+ 	th { font-size: 1.2em; text-align: left; border: none; padding-left: 19; }
  	td { padding: 0.25em 5em 0.50em 1em; border: 0 none; }
  </style>
  </head>
