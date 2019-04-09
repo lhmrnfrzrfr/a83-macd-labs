bf43ab6f66edc1ad720bae9201009dcad48788c8
unknown <ilhamriana3@gmail.com>
2019-04-10 03:15:16 +0700

    penambahan table row

--- index.php
+++ index.php
@@ -49,7 +49,7 @@
             $sql_insert = "INSERT INTO Registration (nim, name, email, jurusan, date) 
                         VALUES (?,?,?,?,?)";
             $stmt = $conn->prepare($sql_insert);
-            $stmt->bindValue(1, $name);
+            $stmt->bindValue(1, $nim);
             $stmt->bindValue(2, $name);
             $stmt->bindValue(3, $email);
             $stmt->bindValue(4, $jurusan);
